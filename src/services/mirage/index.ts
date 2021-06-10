99b170f512e118b06682c0547dcdbe4c7d46a73b
rogerrm95 <rogerrm@live.com>
2021-06-10 20:16:16 -0300

    Config: adiciona e configura o MirageJS á aplicação #2

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,16 +1,35 @@
-import { createServer, Model } from 'miragejs'
+import { createServer, Factory, Model } from 'miragejs'
+import faker from 'faker'
 
 type User = {
     name: string,
     email: string,
-    created_at: string 
+    created_at: string
 }
 
 export function makeServer() {
     const server = createServer({
         // Banco de dados - fictício //
         models: {
-            user: Model.extend<Partial<User>>({}) 
+            user: Model.extend<Partial<User>>({})
+        },
+
+        factories: {
+            user: Factory.extend({
+                name(index: number) {
+                    return `User ${index + 1}`
+                },
+                email() {
+                    return faker.internet.email().toLowerCase()
+                },
+                createdAt() {
+                    return faker.date.recent(10, new Date())
+                }
+            })
+        },
+
+        seeds(server) {
+            server.createList('user', 200)
         },
 
         routes() {
